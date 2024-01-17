f735bea52c0c28bc0f665caa1317e32fa1ad7ecf
Xiaowei Xue <xiaowei.xue@shopee.com>
2024-01-17 17:09:19 +0800

    fix: lint issue

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,7 +10,7 @@ import {
   User,
   Revenue,
 } from './definitions';
-import { formatCurrency } from "./utils";
+import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
